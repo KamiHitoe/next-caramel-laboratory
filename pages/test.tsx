01b19f5b6d8723c2670f9425f310ca5ef6dc96fb
KamiHitoe <sousaku_0912@yahoo.co.jp>
2022-03-17 18:35:50 +0900

    add recoil test

--- pages/test.tsx
+++ pages/test.tsx
@@ -1,15 +1,71 @@
-import * as React from 'react'
-import type { NextPage } from 'next'
-import styles from '../styles/Test.module.scss'
-import Button from '@mui/material/Button'
+import React from 'react';
+import {
+  RecoilRoot,
+  atom,
+  selector,
+  useRecoilState,
+  useRecoilValue,
+} from 'recoil';
 
-const Foo: NextPage = () => {
+// main React component
+function App() {
   return (
-    <div className={styles.title}>
-      <h1>test</h1>
-      <Button variant="contained">Contained</Button>
-    </div>
-  )
+    <RecoilRoot>
+      <CharacterCounter />
+    </RecoilRoot>
+  );
 }
 
-export default Foo
+// function component
+function CharacterCounter() {
+  return (
+    <div>
+      <h1>Test Recoil</h1>
+      <TextInput />
+      <CharacterCount />
+    </div>
+  );
+}
+
+// stateの定義, store的な？
+const textState = atom({
+  key: 'textState', // unique ID (with respect to other atoms/selectors)
+  default: '', // default value (aka initial value)
+});
+
+// stateの定義
+function TextInput() {
+  const [text, setText] = useRecoilState(textState);
+
+  // define event handler
+  const onChange = (event) => {
+    setText(event.target.value); // input value
+  };
+
+  return (
+    <div>
+      <input type="text" value={text} onChange={onChange} />
+      <br />
+      Echo: {text}
+    </div>
+  );
+}
+
+const charCountState = selector({
+  key: 'charCountState', // unique ID (with respect to other atoms/selectors)
+  get: ({get}) => {
+    const text = get(textState);
+
+    return text.length;
+  },
+});
+
+function CharacterCount() {
+  const count = useRecoilValue(charCountState);
+
+  return <>Character Count: {count}</>;
+}
+
+export default App
+
+
